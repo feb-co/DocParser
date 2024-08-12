29afd4d42f1502bcdb9ec68cd1ec64157fa5156d
Licheng Wang <244267620@qq.com>
2024-08-12 18:54:07 +0000

    disable ALBUMENTATIONS logging

--- api/functional/pdf_parser.py
+++ api/functional/pdf_parser.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
+
 work_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(work_dir)
 
